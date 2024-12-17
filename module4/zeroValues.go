0c8c8fcfaef049dc5fefe31f1d8a25b5787a60c3
oGalhardo <juniorgalhardo03@gmail.com>
2024-12-17 12:59:26 -0300

    Funcoes

---
+++ module4/zeroValues.go
@@ -0,0 +1,11 @@
+package main
+
+// import "fmt"
+
+// func main() {
+// 	var i int
+// 	var f float64
+// 	var b bool
+// 	var s string
+// 	fmt.Printf("inteiro: %v\n float: %v\n bool: %v\n string: %v\n", i, f, b, s)
+// }
