73c68c70595cf1019d78a7d59efce02ea0b5593c
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-13 12:57:11 -0300

    ✨ Binding

--- src/model/request/user_request.go
+++ src/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"passoword"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
+	Email    string `json:"email" binding:""`
+	Password string `json:"passoword" binding:""`
+	Name     string `json:"name" binding:"required,min=4,max=50"`
+	Age      int8   `json:"age" binding:""`
 }
