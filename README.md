9a474ed00e933218c8162165ac0063994ccfb349
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-20 12:58:02 -0300

    🐛 Teste

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # EstudandoGo
 
 Seguindo aula de [MEU_PRIMEIRO_CRUD] https://www.youtube.com/watch?v=h2aZ1Ffbn80&list=PLm-xZWCprwYQ3gyCxJ8TR1L2ZnUOPvOpr&index=8
+Seguindo https://www.youtube.com/watch?v=QT7YvrEWMGE&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=9
--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"log"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"github.com/oGalhardo/EstudandoGo/src/controller/routes"
+	"fmt"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error loading .env file")
-	}
-	router := gin.Default()
-	routes.InitRoutes(&router.RouterGroup)
-	if err := router.Run(":9090"); err != nil {
-		log.Fatal(err)
-	}
+	x := 19
+	fmt.Printf("x: %v, %T\n", x, x)
 }
