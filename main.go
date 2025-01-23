9a3edfc8c2d2a2ffe897ccb8ec19abb09b2cc470
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-23 12:59:48 -0300

    ✨ Exercicie

--- main.go
+++ main.go
@@ -1,14 +1,45 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
-type ol int
+// type ol int
 
-var b ol
+// var b ol
+
+// func ex1() {
+// x := 42
+// y := "James Bond"
+// z := true
+// fmt.Println(x, y, z)
+// fmt.Println(x)
+// fmt.Println(z)
+// fmt.Println(y)
+// }
+
+// func ex2() {
+// 	fmt.Println(x, y, z)
+// 	fmt.Println("Esses valores se chamam:valores zero")
+// }
+
+// var x int = 4
+// var y string = " James Bond"
+// var z bool
+
+// func ex3() {
+// 	s := fmt.Sprint(x, y, z)
+// 	fmt.Println(s)
+// }
+
+type top int
+
+var x top
+
+func ex4() {
+	fmt.Printf("valor de x=%v e seu tipo %T", x, x)
+	x = 42
+	fmt.Println(x)
+}
 
 func main() {
-	fmt.Println("Type of variable5:", reflect.TypeOf(b))
+	ex4()
 }
