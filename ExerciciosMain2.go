2721dc556749ad98a8df998228b9e0c2ae73f7ed
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-28 13:00:18 -0300

    🩹 Nivel 2

---
+++ ExerciciosMain2.go
@@ -0,0 +1,31 @@
+package main
+
+// func ex1() {
+// 	int_value := 21
+// 	fmt.Printf("hex value as %#x\n", int_value)
+// 	fmt.Printf("decimal value as %v\n", int_value)
+// 	fmt.Printf("decimal value as %b\n", int_value)
+
+// }
+
+// func ex2() {
+// 	const (
+// 		a = 10
+// 		b = 21
+// 	)
+// 	res := a == b
+// 	res2 := a >= b
+// 	res3 := a <= b
+// 	res4 := a > b
+// 	res5 := a < b
+// 	res6 := a != b
+// 	fmt.Printf("res: %v \n res2: %v\n , res3: %v \n , res4: %v \n , res5: %v , res6:%v", res,
+// 		res2,
+// 		res3, res4, res5, res6,
+// 	)
+
+// }
+
+// func main() {
+// 	ex2()
+// }
--- main2.go
+++ main2.go
@@ -8,5 +8,63 @@ package main
 
 // }
 
-func main() {
-}
+// const x = 10
+// const (
+// 	e = 10
+// 	f = 10
+// 	j = true
+// )
+
+// var y = 10
+// var c int
+// var d float64
+
+// func CONSTANTE() {
+// 	c = y
+// 	d = x
+// 	fmt.Println(e, f, j)
+// }
+
+// const (
+// 	x = iota
+// 	y = iota
+// 	z = iota
+// )
+
+// const (
+// 	a = iota
+// 	_ = iota
+// 	c = iota
+// )
+
+// const (
+// 	d = iota
+// 	_
+// 	e
+// )
+
+// const (
+// 	de = iota + 100
+// )
+
+// func IOTA() {
+// 	fmt.Println(x, y, z)
+// 	fmt.Println(a, c)
+// 	fmt.Println(d, e)
+// 	fmt.Println(de)
+
+// }
+
+// func delocaBits() {
+// 	x := 2
+// 	y := x << 1
+// 	d := y >> 1
+// 	fmt.Println(x)
+// 	fmt.Println(y)
+// 	fmt.Println(d)
+
+// }
+
+// func main() {
+// 	delocaBits()
+// }
