ed6f0e9b9c4259171f2b40727c9e3c837cd1c068
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-29 12:58:02 -0300

    ✨ Bew Chapter

--- ExerciciosMain2.go
+++ ExerciciosMain2.go
@@ -1,31 +1,63 @@
 package main
 
-// func ex1() {
-// 	int_value := 21
-// 	fmt.Printf("hex value as %#x\n", int_value)
-// 	fmt.Printf("decimal value as %v\n", int_value)
-// 	fmt.Printf("decimal value as %b\n", int_value)
+// import "fmt"
 
-// }
+//  func ex1() {
+//  	int_value := 21
+//  	fmt.Printf("hex value as %#x\n", int_value)
+//  	fmt.Printf("decimal value as %v\n", int_value)
+//  	fmt.Printf("decimal value as %b\n", int_value)
 
-// func ex2() {
+//  }
+
+//  func ex2() {
+//  	const (
+//  		a = 10
+//  		b = 21
+//  	)
+//  	res := a == b
+//  	res2 := a >= b
+//  	res3 := a <= b
+//  	res4 := a > b
+//  	res5 := a < b
+//  	res6 := a != b
+//  	fmt.Printf("res: %v \n res2: %v\n , res3: %v \n , res4: %v \n , res5: %v , res6:%v", res,
+//  		res2,
+//  		res3, res4, res5, res6,
+//  	)
+
+//  }
+
+//  const j = 10
+
+//  const i string = "string"
+
+//  func ex3() {
+//  	fmt.Println(i, j)
+//  }
+
+// 	func ex4() {
+// 		const i = 200
+// 		fmt.Printf("decimal = %d\n binario = %b\n hexadecimal = %#x\n", i, i, i)
+// 		const bitsLeft = i << 1
+// 		fmt.Printf("Bits Deslocados para esquerda = %v\n", bitsLeft)
+// 		fmt.Printf("decimal = %d\n binario = %b\n hexadecimal = %#x\n", bitsLeft, bitsLeft, bitsLeft)
+// 	}
+
+//  func ex5() {
+//  	const msg string = `"PGentlemena
+
+//  	Goly"
+//  	`
+//  	fmt.Println(msg)
+//  }
+
+// func ex6() {
 // 	const (
-// 		a = 10
-// 		b = 21
+// 		ano  = iota + 2025
+// 		ano2 = iota + 2025
+// 		ano3 = iota + 2025
+// 		ano4 = iota + 2025
 // 	)
-// 	res := a == b
-// 	res2 := a >= b
-// 	res3 := a <= b
-// 	res4 := a > b
-// 	res5 := a < b
-// 	res6 := a != b
-// 	fmt.Printf("res: %v \n res2: %v\n , res3: %v \n , res4: %v \n , res5: %v , res6:%v", res,
-// 		res2,
-// 		res3, res4, res5, res6,
-// 	)
-
-// }
-
-// func main() {
-// 	ex2()
+// 	fmt.Println(ano, ano2, ano3, ano4)
 // }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EstudandoGo
 
 Seguindo aula de [MEU_PRIMEIRO_CRUD] https://www.youtube.com/watch?v=h2aZ1Ffbn80&list=PLm-xZWCprwYQ3gyCxJ8TR1L2ZnUOPvOpr&index=8
-Seguindo https://www.youtube.com/watch?v=12o81rTFu_w&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=33
+Seguindo https://www.youtube.com/watch?v=g_Qdxi1b2cE&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=39
