9d2fa5b6a83a6c7bbaf833a1db2914e7a9db3b4f
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-30 12:55:50 -0300

    🐛 Corrige

--- FluxoDeControle.go
+++ FluxoDeControle.go
@@ -1,1 +1,35 @@
 package main
+
+import "fmt"
+
+// func aula1() {
+// 	for x := 0; x < 10; x++ {
+// 		fmt.Println(x)
+// 	}
+// }
+
+func ex1() {
+	i := 1
+	for i <= 3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+	for j := 6; j <= 9; j++ {
+		fmt.Println(j)
+	}
+	for {
+		fmt.Println("loop")
+		break
+
+	}
+	for n := 0; n <= 5; n++ {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+}
+
+func main() {
+	ex1()
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EstudandoGo
 
 Seguindo aula de [MEU_PRIMEIRO_CRUD] https://www.youtube.com/watch?v=h2aZ1Ffbn80&list=PLm-xZWCprwYQ3gyCxJ8TR1L2ZnUOPvOpr&index=8
-Seguindo https://www.youtube.com/watch?v=g_Qdxi1b2cE&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=39
+Seguindo https://www.youtube.com/watch?v=EL9wo6Zrz9o&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=40
