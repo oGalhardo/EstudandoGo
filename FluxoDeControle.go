9094c4cafcb4b814c389e368bd9f3593eea39c9d
oGalhardo <juniorgalhardo03@gmail.com>
2025-01-31 12:55:47 -0300

    ✨ Loop Nested

--- FluxoDeControle.go
+++ FluxoDeControle.go
@@ -1,35 +1,42 @@
 package main
 
-import "fmt"
-
 // func aula1() {
 // 	for x := 0; x < 10; x++ {
 // 		fmt.Println(x)
 // 	}
 // }
 
-func ex1() {
-	i := 1
-	for i <= 3 {
-		fmt.Println(i)
-		i = i + 1
-	}
-	for j := 6; j <= 9; j++ {
-		fmt.Println(j)
-	}
-	for {
-		fmt.Println("loop")
-		break
+// func ex1() {
+// 	i := 1
+// 	for i <= 3 {
+// 		fmt.Println(i)
+// 		i = i + 1
+// 	}
+// 	for j := 6; j <= 9; j++ {
+// 		fmt.Println(j)
+// 	}
+// 	for {
+// 		fmt.Println("loop")
+// 		break
 
-	}
-	for n := 0; n <= 5; n++ {
-		if n%2 == 0 {
-			continue
-		}
-		fmt.Println(n)
-	}
-}
+// 	}
+// 	for n := 0; n <= 5; n++ {
+// 		if n%2 == 0 {
+// 			continue
+// 		}
+// 		fmt.Println(n)
+// 	}
+// }
+
+// func exNestedLoop() {
+// 	for mes := 1; mes <= 12; mes++ {
+// 		fmt.Println("Mes", mes)
+// 		for x := 1; x <= 30; x++ {
+// 			fmt.Print("Dia: ", x, ", ")
+// 		}
+// 		fmt.Print("\n")
+// 	}
+// }
 
 func main() {
-	ex1()
 }
