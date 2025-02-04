fb7bdb51189848950dd15414af83b0ac76dc51c5
oGalhardo <juniorgalhardo03@gmail.com>
2025-02-04 12:57:57 -0300

    🚑️ Exericcios

--- FluxoControle2.go
+++ FluxoControle2.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 // func ex1() {
 // 	x := 10
 // 	if x < 10 {
@@ -17,17 +15,47 @@ import "fmt"
 // 	}
 // }
 
-func extensionsIf() {
-	if x := 5; x > 10 {
-		fmt.Println("Is maior que 10")
-	} else if x < 1 {
-		fmt.Println("X < 10")
-	} else {
-		fmt.Println("500")
+// func extensionsIf() {
+// 	if x := 5; x > 10 {
+// 		fmt.Println("Is maior que 10")
+// 	} else if x < 1 {
+// 		fmt.Println("X < 10")
+// 	} else {
+// 		fmt.Println("500")
 
-	}
-}
+// 	}
+// }
 
-func main() {
-	extensionsIf()
-}
+// func switch_expression() {
+// 	x := 2
+// 	switch x {
+// 	case 3, 2:
+// 		fmt.Println("TOP")
+// 		fallthrough
+// 	default:
+// 		fmt.Println("oba")
+// 	}
+
+// }
+
+// var d interface{}
+// func switch_expression2() {
+// 	d = "s"
+// 	switch d.(type) {
+// 	case int:
+// 		fmt.Println(("BOA"))
+// 	case string:
+// 		fmt.Println(d)
+// 	}
+// }
+
+// func switch_expression3() {
+// 	switch x := "2"; {
+// 	case x == "2":
+// 		fmt.Println(x)
+// 	}
+// }
+
+// func main() {
+// 	switch_expression3()
+// }
---
+++ OperadoresLógicos.go
@@ -0,0 +1,15 @@
+package main
+
+// func ex1() {
+// 	for i := 0; i < 10001; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
+
+func ex2(){
+	ALFABETO:=["a","b"]
+}
+
+func main() {
+	ex1()
+}
