9a783b63a42a284d1dad166e44a040d6f0fe062c
oGalhardo <juniorgalhardo03@gmail.com>
2025-02-07 12:58:28 -0300

    🚑️ Struct

---
+++ Maps.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+// func ex1() {
+// 	amigos := map[string]int{
+// 		"alfredo": 123,
+// 		"joana":   999,
+// 	}
+// 	amigos["pato"] = 123
+// 	total := "2"
+// 	for key, amigo := range amigos {
+// 		total += key
+// 		fmt.Printf("%v \n %v", amigo, key)
+// 	}
+// 	fmt.Println(total)
+// fmt.Println(amigos)
+
+// sera, ok := amigos["fantasma"]
+// if !ok {
+// 	amigos["fantasma"] = 99
+// } else {
+// 	fmt.Println("fantasma")
+// }
+///Não existe fantasma no amigos
+//Ok retorna um bool dizendo se o valor é vdd ou false
+// fmt.Println(sera, ok)
+// }
+
+type x struct {
+}
+
+func main() {
+	ex1()
+}
