c3b16f09c5b5bb3e962a4bfcce3d8d16340b5974
oGalhardo <juniorgalhardo03@gmail.com>
2025-02-20 13:11:44 -0300

    🐛 Continue curs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # EstudandoGo
 
-Seguindo aula de [MEU_PRIMEIRO_CRUD] https://www.youtube.com/watch?v=-71BdpuaEks&list=PLm-xZWCprwYQ3gyCxJ8TR1L2ZnUOPvOpr&index=3
+Seguindo aula de [MEU_PRIMEIRO_CRUD] https://www.youtube.com/watch?v=-71BdpuaEks&list=PLm-xZWCprwYQ3gyCxJ8TR1L2ZnUOPvOpr&index=4
 Seguindo https://www.youtube.com/watch?v=PPOBe49M8V0&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg&index=87
--- main.go
+++ main.go
@@ -1,73 +1,17 @@
 package main
 
-// type ol int
+import (
+	"fmt"
+	"log"
+	"os"
 
-// var b ol
+	"github.com/joho/godotenv"
+)
 
-// func ex1() {
-// x := 42
-// y := "James Bond"
-// z := true
-// fmt.Println(x, y, z)
-// fmt.Println(x)
-// fmt.Println(z)
-// fmt.Println(y)
-// }
-
-// func ex2() {
-// 	fmt.Println(x, y, z)
-// 	fmt.Println("Esses valores se chamam:valores zero")
-// }
-
-// var x int = 4
-// var y string = " James Bond"
-// var z bool
-
-// func ex3() {
-// 	s := fmt.Sprint(x, y, z)
-// 	fmt.Println(s)
-// }
-
-// type top int
-
-// var x top
-
-// func ex4() {
-// 	fmt.Printf("valor de x=%v e seu tipo %T", x, x)
-// 	x = 42
-// 	fmt.Println(x)
-// }
-
-// var y int
-
-// func ex5() {
-// 	fmt.Printf("Valor de x: %v\ntipo de x:%T\n", x, x)
-// 	x = 42
-// 	y = int(x)
-// 	fmt.Printf("Valor de y: %v\ntipo de x:%T", y, y)
-
-// }
-
-// func aulaTypesNum() {
-// 	a := "e"
-// 	b := "É"
-// 	fmt.Printf("%v,%v\n", a, b)
-// 	d := []byte(a)
-// 	e := []byte(b)
-// 	fmt.Printf("%v, %v\n", d, e)
-// 	fmt.Println(runtime.GOOS)
-// 	fmt.Println(runtime.GOARCH)
-
-// }
-
-// func OVERFLOW() {
-// 	var i uint16
-// 	i = 65535
-// 	fmt.Println(i)
-// 	i++
-// 	fmt.Println(i)
-// }
-
-// func main() {
-// 	OVERFLOW()
-// }
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env")
+	}
+	fmt.Println(os.Getenv("TEST"), "ENV")
+}
